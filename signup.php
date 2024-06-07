84759ed7dd6279b522e6fa50a24f91eb05b119d4
ILAGAN, ANDREI JIREH M. <ilaganandreijireh@gmail.com>
2024-06-08 01:08:29 +0800

    add new changes

--- signup.php
+++ signup.php
@@ -7,21 +7,23 @@
   </head>
   <body>
     <div class="container">
+      <div class="form-section">
+        <form>
+          <input type="email" placeholder="Email" required>
+          <input type="text" placeholder="Last Name" required>
+          <input type="text" placeholder="First Name" required>
+          <input type="password" placeholder="Password" required>
+          <input type="date" placeholder="Date of Birth" required>
+          <button type="submit" class="submit-btn">
+            <span class="submit-text">Register</span>
+          </button>
+        </form>
+      </div>
       <div class="welcome-section">
         <h1>MemoirVerse Registration</h1>
         <p>Enter your personal details to use all of site features.</p>
       </div>
-      <form>
-      <input type="email" placeholder="Email" required>
-      <input type="text" placeholder="Last Name" required>
-      <input type="text" placeholder="First Name" required>
-      <input type="password" placeholder="Password" required>
-      <input type="date" placeholder="Date of Birth" required>
-      <button type="submit">Register</button>
-    </form>
-  </div>
-        <div class="separator">or</div>
-      </div>
     </div>
   </body>
 </html>
+
